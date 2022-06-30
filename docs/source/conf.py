0eceed123633bed3bd129d80d5183ba0b69b03f9
jorrick <jorrick.sleijster@adyen.com>
2022-06-30 22:35:06 +0200

    FIX readthedocs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ author = "Jorrick Sleijster"
 
 # The full version, including alpha/beta/rc tags
 release = "0.1.0a1"
+version = "0.1.0a1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -32,6 +33,7 @@ release = "0.1.0a1"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "autoapi.extension",
     "sphinx_rtd_theme",
     # "sphinx.ext.autodoc",
     "sphinx.ext.autosectionlabel",
@@ -42,7 +44,6 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
 ]
-extensions.append("autoapi.extension")
 
 autoapi_type = "python"
 autoapi_dirs = [(pathlib.Path(__file__).parents[2].resolve() / "src").as_posix()]
@@ -55,6 +56,12 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -66,7 +73,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # Add some SPHINX specific options
 html_theme_options = {
