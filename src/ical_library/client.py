65e6452764d02e8693f9890e8b149e09530ca44f
jorrick <jorrick.sleijster@adyen.com>
2022-07-15 23:02:58 +0200

    🐛 Clients unable to handle carriage return

--- src/ical_library/client.py
+++ src/ical_library/client.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 from typing import Any, List, Union
 from urllib import request
@@ -5,12 +6,17 @@ from urllib import request
 from ical_library.ical_components import VCalendar
 
 
-def parse_lines_into_calendar(lines: List[str]) -> VCalendar:
+def parse_lines_into_calendar(raw_text: str) -> VCalendar:
     """
     Given the lines of an iCalendar file, return a parsed VCalendar instance.
-    :param lines: The lines of the iCalendar file/website.
+    :param raw_text: The raw text of the iCalendar file/website.
     :return: a VCalendar with all it's iCalendar components like VEvents, VToDos, VTimeZones etc.
     """
+    lines: List[str] = []
+    for line in re.split(r"\n", raw_text):
+        line_content = re.sub(r"^\n|^\r|\n$|\r$", "", line)
+        if len(line_content) > 0:
+            lines.append(line_content)
     new_instance = VCalendar()
     if lines[0] != "BEGIN:VCALENDAR":
         raise ValueError(f"This is not a ICalendar as it started with {lines[0]=}.")
@@ -25,7 +31,7 @@ def parse_icalendar_file(file: Union[str, Path]) -> VCalendar:
     :return: a VCalendar instance with all it's iCalendar components like VEvents, VToDos, VTimeZones etc.
     """
     with open(file, "r") as ical_file:
-        return parse_lines_into_calendar([line.strip("\n") for line in ical_file.readlines()])
+        return parse_lines_into_calendar(ical_file.read())
 
 
 def parse_icalendar_url(url: str, **kwargs: Any) -> VCalendar:
@@ -37,5 +43,4 @@ def parse_icalendar_url(url: str, **kwargs: Any) -> VCalendar:
     """
     response = request.urlopen(url, **kwargs)
     text = response.read().decode("utf-8")
-    lines = text.split("\n")
-    return parse_lines_into_calendar(lines)
+    return parse_lines_into_calendar(text)
